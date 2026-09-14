ed23b99662c6a0f7697ddcef9878921a4994e777
robertot89 <robertot1989@gmail.com>
2026-09-14 13:24:41 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.plugin.compose")
+    id("com.android.application") version "9.4.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.3.21" apply false
 }
